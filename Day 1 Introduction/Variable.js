8d18f94f07c62b23463a8da3dd3296a4212ec682
Matiyas Moges <engmatiyasmogest@gmail.com>
2023-11-25 04:13:32 +0300

    saved

---
+++ Day 1 Introduction/Variable.js
@@ -0,0 +1,4 @@
+let myname = 'Matiyas'
+let we = true;
+let ourname
+let theirname = null
---
+++ Day 1 Introduction/datatypes.js
@@ -0,0 +1,21 @@
+a= typeof 'Matiyas'
+b= typeof true
+c= typeof null
+d= typeof undefined
+let kiya
+let we
+let them
+let four
+let kim = 'kim'
+const saka = 7
+let arsenal = true
+const ucl = 1
+let firstname = 'Matiyas'
+let lastname = 'Moges'
+let marital status = 'single'
+let country = 'Ethiopia'
+const age = 20
+let firstname = 'Matiyas', lastname = 'Moges', marital status = 'single', country = 'Ethiopia', age = 20
+let myAge = 20
+let yourAge = 19
+console.log(myAge, yourAge)
